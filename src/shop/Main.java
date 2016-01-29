a27bc8a8e8cd8f1a7f47882a5cf2ffa61b6845c6
Daniel Kucal <dkucal@gmail.com>
2016-01-29 19:53:58 +0100

    main.fxml move and tidy up

--- src/shop/Main.java
+++ src/shop/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("main.fxml"));
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("view/main.fxml"));
         loader.setController(new HomeController());
         Parent root = loader.load();
         primaryStage.setTitle("JavaShop");
